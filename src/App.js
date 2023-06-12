2fe54abb48c57a5a5e0eb76cd401d7be9bd7ce78
Melinda Mott <melinda.mott@gmail.com>
2023-06-12 15:00:00 -0700

    Added Animal List component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 import Animal from './components/Animal';
+import AnimalList from './components/AnimalList';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <h2> Animal Listings</h2>
       {/* I want to list a bunch of animals.. */}
       {/* Each animal has its name and photo */}
+      <AnimalList></AnimalList>
       <Animal></Animal>
       <Animal></Animal><Animal></Animal>
     </section>
